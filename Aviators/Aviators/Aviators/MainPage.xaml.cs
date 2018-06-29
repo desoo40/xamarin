56131b7c3436cd80dd7eed7338090d32a6b2acdd
Denis Sorokin <deso40@gmail.com>
2018-06-29 06:29:09 +0300

    start AviProj

---
+++ Aviators/Aviators/Aviators/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Aviators
+{
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void ButClick(object sender, EventArgs e)
+        {
+
+
+        }
+
+	}
+}
